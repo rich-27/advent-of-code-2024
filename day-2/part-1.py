03880e318823b109db93fce8b6a9138f0e43f76f
rich-27 <rmlewis14@gmail.com>
2024-12-03 20:06:13 +0000

    Remove unused import

--- day-2/part-1.py
+++ day-2/part-1.py
@@ -1,12 +1,18 @@
-import pandas as pd
 import numpy as np
 
-reactor_reports = [np.array(report.split(), dtype=int) for report in np.loadtxt("day-2/input.txt", dtype=str, delimiter=',')]
+reactor_reports = [
+    np.array(report.split(), dtype=int)
+    for report in np.loadtxt("day-2/input.txt", dtype=str, delimiter=",")
+]
+
 
 def check_safety(report):
     differences = np.subtract(report[1:], report[:-1])
     signs, values = (np.sign(differences), np.abs(differences))
-    return np.all(signs == signs[0]) and np.all(np.logical_and(values >= 1, values <= 3))
+    return np.all(signs == signs[0]) and np.all(
+        np.logical_and(values >= 1, values <= 3)
+    )
+
 
 safe_report_count = np.sum([1 if check_safety(row) else 0 for row in reactor_reports])
 print(safe_report_count)
