9c5db89428e1d31aef219d00de7d523d7f3ca6b3
rich-27 <rmlewis14@gmail.com>
2024-12-06 10:47:40 +0000

    Formatted day-6/part-1

--- day-6/part-1.py
+++ day-6/part-1.py
@@ -1,30 +1,45 @@
 import numpy as np
 
-lab_map = np.genfromtxt('day-6/input.txt', dtype=str, delimiter=1, comments=None)
+lab_map = np.genfromtxt("day-6/input.txt", dtype=str, delimiter=1, comments=None)
 
-guard_location = np.array([indices[0] for indices in np.where(lab_map == '^')])
+guard_location = np.array([indices[0] for indices in np.where(lab_map == "^")])
 
 # Guard direction is first row of array
-guard_direction = np.rec.fromrecords([(direction, move) for direction, move in {'N': (-1, 0), 'E': (0, 1), 'S': (1, 0), 'W': (0, -1)}.items()], dtype=[('name', 'U1'), ('move', tuple)])
+guard_direction = np.rec.fromrecords(
+    [
+        (direction, move)
+        for direction, move in {
+            "N": (-1, 0),
+            "E": (0, 1),
+            "S": (1, 0),
+            "W": (0, -1),
+        }.items()
+    ],
+    dtype=[("name", "U1"), ("move", tuple)],
+)
+
 
 def location_on_map(guard_location):
-    return np.all([index in range(size) for index, size in zip(guard_location, lab_map.shape)])
+    return np.all(
+        [index in range(size) for index, size in zip(guard_location, lab_map.shape)]
+    )
+
 
 is_on_map = True
 while is_on_map:
     next_location = np.add(guard_location, guard_direction[0].move)
     is_on_map = location_on_map(next_location)
     if not is_on_map:
-        lab_map[*guard_location] = 'X'
+        lab_map[*guard_location] = "X"
         # Reached border
         break
 
     match lab_map[*next_location]:
-        case '#':
+        case "#":
             guard_direction = np.roll(guard_direction, -1, axis=0)
-        case '.' | 'X':
-            lab_map[*guard_location] = 'X'
+        case "." | "X":
+            lab_map[*guard_location] = "X"
             guard_location = next_location
 
-locations_visited = (lab_map == 'X').sum()
+locations_visited = (lab_map == "X").sum()
 print(locations_visited)
