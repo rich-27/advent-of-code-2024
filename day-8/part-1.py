eacd552e2cf1bad5543d3b27e1662462aa89c181
rich-27 <rmlewis14@gmail.com>
2024-12-08 10:30:36 +0000

    Format part-1 of day-8

--- day-8/part-1.py
+++ day-8/part-1.py
@@ -1,13 +1,13 @@
 import numpy as np
 from itertools import combinations
 
-antenna_map = np.genfromtxt('day-8/input.txt', dtype=str, delimiter=1)
+antenna_map = np.genfromtxt("day-8/input.txt", dtype=str, delimiter=1)
 
 indices = np.moveaxis(np.indices(antenna_map.shape), 0, -1)
 
-unique_antennas = np.sort(np.unique(antenna_map[antenna_map != '.']))
+unique_antennas = np.sort(np.unique(antenna_map[antenna_map != "."]))
 
-map_layers = np.full([unique_antennas.size, *antenna_map.shape], '.')
+map_layers = np.full([unique_antennas.size, *antenna_map.shape], ".")
 
 for layer_index, layer_antenna in enumerate(unique_antennas):
     layer_filter = antenna_map == layer_antenna
@@ -16,11 +16,14 @@ for layer_index, layer_antenna in enumerate(unique_antennas):
     for antenna_index_pair in combinations(indices[layer_filter], 2):
         difference = np.subtract(*antenna_index_pair)
 
-        for antinode_index in [np.add(antenna_index_pair[0], difference), np.subtract(antenna_index_pair[1], difference)]:
+        for antinode_index in [
+            np.add(antenna_index_pair[0], difference),
+            np.subtract(antenna_index_pair[1], difference),
+        ]:
             if not np.any(np.all(indices == antinode_index, axis=2)):
                 continue
 
-            map_layers[layer_index, *antinode_index] = '#'
+            map_layers[layer_index, *antinode_index] = "#"
 
-unique_antinode_positions = np.any(map_layers == '#', axis=0).sum()
+unique_antinode_positions = np.any(map_layers == "#", axis=0).sum()
 print(unique_antinode_positions)
