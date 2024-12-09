2003f09a9564c5ba9450424a0c9af6bb2c28297f
rich-27 <rmlewis14@gmail.com>
2024-12-09 11:29:45 +0000

    Remove unused variables

--- day-9/part-1.py
+++ day-9/part-1.py
@@ -2,8 +2,6 @@ import numpy as np
 
 dense_file_map = np.append(np.genfromtxt("day-9/input.txt", dtype=int, delimiter=1), [0]).reshape([-1, 2])
 
-blocks, free_space = np.sum(dense_file_map, axis=0)
-
 file_map = [block for index, (file_size, free_size) in enumerate(dense_file_map) for block in [*(index for _ in range(file_size)), *(-1 for _ in range(free_size))]]
 
 def scan(head, tail):
