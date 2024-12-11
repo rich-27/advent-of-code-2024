b37d0de3de4a2008294a1772face13a3121fa70d
rich-27 <rmlewis14@gmail.com>
2024-12-11 11:08:55 +0000

    Solve part-1 of day-11

---
+++ day-11/part-1.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+stones = np.genfromtxt("day-11/input.txt", dtype=int, delimiter=' ').tolist()
+
+def iterate_stone(stone):
+    if stone == 0:
+        return [1]
+    
+    stone_string = str(stone)
+    if len(stone_string) % 2 == 0:
+        middle = len(stone_string) // 2
+        return [int(stone_string[:middle]), int(stone_string[middle:])]
+    
+    return [stone * 2024]
+
+def iterate_stones(stones):
+    return [new_stone for stone in stones for new_stone in iterate_stone(stone)]
+
+for _ in range(25):
+    stones = iterate_stones(stones)
+
+print(len(stones))
