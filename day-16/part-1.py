ae4a30dd72da39b5bb37930e1126b090e525fcca
rich-27 <rmlewis14@gmail.com>
2024-12-16 19:58:05 +0000

    Refactor part-1 of day-16 for efficiency

--- day-16/part-1.py
+++ day-16/part-1.py
@@ -2,94 +2,68 @@ import numpy as np
 
 
 def pathfind(starting_location):
-    location_scores = {
-        move: np.empty(reindeer_maze.shape, dtype=int) for move in move_lookup.keys()
-    }
-
-    paths = [
-        (0, (starting_location[None, ...], [">"])),
-        *(
-            (1000, (starting_location[None, ...], [direction]))
-            for direction in ["v", "^"]
-        ),
-        (2000, (starting_location[None, ...], ["<"])),
-    ]
+    starting_id = (tuple(starting_location.tolist()), ">")
+    paths = {starting_id: {0: [(starting_location[None, ...], [">"])]}}
+    path_scores = [(0, starting_id)]
     complete_paths = {}
-    min_score = None
 
-    while len(paths) > 0:
-        score, (locations, directions) = paths.pop(0)
+    while len(path_scores) > 0:
+        score, id = path_scores.pop(0)
+        (locations, directions) = paths[id][min(paths[id].keys())][0]
 
-        if min_score is not None and score > min_score:
-            continue
+        for new_direction, turn_score in turns_lookup[directions[-1]].items():
+            next_location = np.add(locations[-1], move_lookup[new_direction])
 
-        print(
-            f"Looking {directions[-1]} from corner ({str(locations[-1][0]): >3}, {str(locations[-1][1]): >3}) (score = {score})..."
-        )
+            symbol = reindeer_maze[*next_location]
+            if symbol == "#" or np.all(locations == next_location, axis=1).any():
+                continue
 
-        if len(complete_paths) > 0 and score < min_score:
-            continue
-
-        while True:
-            next_location = np.add(locations[-1], move_lookup[directions[-1]])
-            if np.all(locations == next_location, axis=1).any():
-                break
-
-            if (
-                location_scores[directions[-1]][*next_location] > 0
-                and location_scores[directions[-1]][*next_location] <= score
-            ):
-                break
-
-            location_scores[directions[-1]][*next_location] = score
-
-            locations = np.append(
-                locations,
-                next_location[None, ...],
-                axis=0,
+            id = (tuple(next_location.tolist()), new_direction)
+            new_score = score + turn_score + 1
+            new_path = (
+                np.append(
+                    locations,
+                    next_location[None, ...],
+                    axis=0,
+                ),
+                [*directions, new_direction],
             )
 
-            score += 1
+            if symbol == "E":
+                if new_score not in complete_paths:
+                    complete_paths[new_score] = [new_path]
+                else:
+                    complete_paths[new_score].append(new_path)
 
-            match reindeer_maze[*locations[-1]]:
-                case "#":
-                    break
+                break
 
-                case "E":
-                    if len(complete_paths) == 0:
-                        min_score = score
-                    else:
-                        min(min_score, score)
+            if id not in paths:
+                paths[id] = {new_score: [new_path]}
+            else:
+                path_score = min(paths[id].keys())
 
-                    if score not in complete_paths:
-                        complete_paths[score] = [directions]
-                    else:
-                        complete_paths[score].append(directions)
-
-                    break
-
-            for move in turns_lookup[directions[-1]]:
-                if reindeer_maze[*np.add(locations[-1], move_lookup[move])] != ".":
+                if new_score > path_score:
                     continue
 
-                insert_index = len(
-                    [
-                        index
-                        for index, (path_score, _) in enumerate(paths)
-                        if path_score <= 1000 + score
+                if new_score not in paths[id]:
+                    paths[id][new_score] = []
+
+                paths[id][new_score].append(new_path)
+
+                if new_score == path_score:
+                    continue
+
+                if new_score < path_score:
+                    path_scores = [
+                        (score, path_id)
+                        for score, path_id in path_scores
+                        if path_id != id
                     ]
-                )
-                paths.insert(
-                    insert_index,
-                    (
-                        score + 1000,
-                        (locations, [*directions, move]),
-                    ),
-                )
 
-            directions.append(directions[-1])
+            path_scores.append((new_score, id))
+            path_scores.sort(key=lambda score_pair: score_pair[0])
 
-    return complete_paths
+    return min(complete_paths.keys())
 
 
 reindeer_maze = np.genfromtxt(
@@ -99,15 +73,11 @@ maze_indicies = np.indices(reindeer_maze.shape)
 
 move_lookup = {">": (0, 1), "v": (1, 0), "<": (0, -1), "^": (-1, 0)}
 turns_lookup = {
-    ">": ["v", "^"],
-    "v": [">", "<"],
-    "<": ["v", "^"],
-    "^": [">", "<"],
+    ">": {">": 0, "v": 1000, "^": 1000, "<": 2000},
+    "v": {"v": 0, ">": 1000, "<": 1000, "^": 2000},
+    "<": {"<": 0, "v": 1000, "^": 1000, ">": 2000},
+    "^": {"^": 0, ">": 1000, "<": 1000, "v": 2000},
 }
 
-paths = pathfind(maze_indicies[:, reindeer_maze == "S"][:, 0])
-# for score, score_paths in paths.items():
-#     for path in score_paths:
-#         print(f"{score}: {path}")
-
-print(min(paths.keys()))
+score = pathfind(maze_indicies[:, reindeer_maze == "S"][:, 0])
+print(score)
